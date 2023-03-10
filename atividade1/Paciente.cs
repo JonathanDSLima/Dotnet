a06fe44630c517afc147ad41c69400c51b553b55
Japa_ <jonathan.lima@maisunifacisa.com.br>
2023-03-10 20:45:19 -0300

    Enviando primeira atividade referente a um mini sistema para uma clínica odontológica

---
+++ atividade1/Paciente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atividade1
+{
+    internal class Paciente
+    {
+        public string Nome { get; private set; }
+        public string CPF { get; private set; }
+
+        public List<string> Procedimentos { get; private set; }
+
+        public  Paciente(string nome, string cpf, List<string> procedimentos)
+        {
+            Nome = nome;
+            CPF = cpf;
+            Procedimentos = procedimentos;  
+        }
+
+    }
+}

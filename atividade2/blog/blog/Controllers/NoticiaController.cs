7bfc04340d65cb64c652517acfe1fac521deb4d1
Jonathan de Souza Lima <jonathandesouzalima@MacBook-Air-de-Jonathan.local>
2023-04-04 21:54:45 -0300

    Envio da segunda atividade avaliativa

---
+++ atividade2/blog/blog/Controllers/NoticiaController.cs
@@ -0,0 +1,58 @@
+﻿using blog.Data;
+using blog.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog.NoticiaController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoticiaController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(Database.noticias);
+        }
+
+        [HttpPost]
+        public IActionResult Add([FromBody] Noticia noticia)
+        {
+            noticia.Id = Guid.NewGuid();
+            Database.noticias.Add(noticia);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(string id)
+        {
+            if (id == null) return NotFound();
+            Noticia? noticia = GetNoticia(id);
+
+            if (noticia == null) return NotFound();
+
+            Database.noticias.Remove(noticia);
+
+            return Ok("Notícia removida com sucesso!");
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Edit(string id, [FromBody] Noticia noticia)
+        {
+            var editNoticia = GetNoticia(id);
+            if (editNoticia == null) return NoContent();
+
+            editNoticia.Titulo = noticia.Titulo;
+            editNoticia.Conteudo = noticia.Conteudo;
+            editNoticia.Autor = noticia.Autor;
+            editNoticia.Data = noticia.Data;
+
+            return Ok(editNoticia);
+        }
+
+        private Noticia? GetNoticia(string id)
+        {
+            return Database.noticias.Where(noticia => noticia.Id.ToString().Equals(id)).FirstOrDefault();
+        }
+    }
+
+}
---
+++ atividade2/blog/blog/Entity/Noticia.cs
@@ -0,0 +1,22 @@
+﻿
+namespace blog.Entity
+{
+	public class Noticia
+	{
+        public Guid Id { get; set; }
+        public string Titulo { get; set; }
+        public string Conteudo { get; set; }
+        public string Autor { get; set; }
+        public DateTime Data { get; set; }
+
+        public Noticia(string titulo, string conteudo, string autor, DateTime data)
+        {
+            Id = Guid.NewGuid();
+            Titulo = titulo;
+            Conteudo = conteudo;
+            Autor = autor;
+            Data = data;
+        }
+    }
+}
+
